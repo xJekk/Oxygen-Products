d6a52b0a60862c9554cc1bb3e191e8f02c79d777
César Fernández <56650023+xJekk@users.noreply.github.com>
2023-03-10 15:45:26 +0100

    Add files via upload

--- Scripts/dom.js
+++ Scripts/dom.js
@@ -1,35 +1,106 @@
 const selectProduct = document.getElementById("number-items");
 const productsContainer = document.getElementById("item-cards");
+const listContainer = document.getElementById("category-list");
+const storage = window.sessionStorage;
+
+const itemCountText = document.getElementById("cart-count");
 
 window.addEventListener("load", (event) => {
+    if (storage.getItem("visit") == null) {
+        storage.setItem("cartCount", 0);
+        storage.setItem("visit", true);
+        itemCountText.innerText = "(0)";
+    } else {
+        itemCountText.innerText = `(${storage.getItem("cartCount")})`;
+    }
     loadProducts(1).then((product) => {
         let productCard = document.createElement('div');
-            productCard.classList.add("item");
-            productCard.innerHTML = `<img src=${product.data[0].image} alt=""/>
-                                          <h2>${product.data[0].name}</h2>
-                                          <p>${product.data[0].description}</p>
-                                          <h3>${Math.round(product.data[0].price)} $</h3>`;
-            productsContainer.appendChild(productCard);
+        productCard.classList.add("item");
+        productCard.innerHTML = `<img src=${product.data[0].image} alt="${product.data[0].name}"/>
+                                 <h2>${product.data[0].name}</h2>
+                                 <p>${product.data[0].description}</p>
+                                 <h3>${Math.round(product.data[0].price)} $</h3>
+                                 <button class="add-btn">Add to cart</button>`;
+                                 
+        productsContainer.appendChild(productCard);
+        let addBtnArray = document.querySelectorAll(".add-btn");
+
+        addBtnArray[0].addEventListener("click", () => {
+            let itemCount = storage.getItem("cartCount");
+            itemCount++;
+            storage.setItem("cartCount", itemCount);
+            itemCountText.innerText = `(${itemCount})`;
+        });
+
+        let categoryObject = getCategoryList(product.data);
+        // Msotramos en la web las categorias de forma dinámicas
+        let listCategory = document.createElement("ul");
+        listCategory.classList.add("category-list-ul");
+        listContainer.appendChild(listCategory);
+        Object.keys(categoryObject).forEach(category => {
+            let filterCategory = document.createElement("li");
+            filterCategory.innerText = `${category} (${categoryObject[category].length})`;
+            listCategory.appendChild(filterCategory);
+            //Elimino evento de clicar porque sólo hay 1 elemento y las categorías que hay son de ese elemento
+        });
     });
   });
 
-selectProduct.addEventListener("change",async () => {
+selectProduct.addEventListener("change", async () => {
     let numProducts = selectProduct.value;
-    productsContainer.innerHTML = "";
+    let listCategory = document.querySelector(".category-list-ul");
+    deleteAllChilds(productsContainer);
+
     loadProducts(numProducts).then((productFromApi) => {
         productFromApi.data.forEach(product => {
             let productCard = document.createElement('div');
             productCard.classList.add("item");
-            productCard.innerHTML = `<img src=${product.image} alt=""/>
+            productCard.innerHTML = `<img src=${product.image} alt="${product.name}"/>
                                      <h2>${product.name}</h2>
                                      <p>${product.description}</p>
-                                     <h3>${Math.round(product.price)} $</h3>`;
+                                     <h3>${Math.round(product.price)} $</h3>
+                                     <button class="add-btn">Add to cart</button>`;
             productsContainer.appendChild(productCard);
+
+            let addBtnArray = document.querySelectorAll(".add-btn");
+
+            addBtnArray.forEach(button => {
+                button.addEventListener("click", () => {
+                    let itemCount = storage.getItem("cartCount");
+                    itemCount++;
+                    storage.setItem("cartCount", itemCount);
+                    itemCountText.innerText = `(${itemCount})`;
+                });
+            });
+            
+            /*let categoryObject = getCategoryList(product.data);
+            Object.keys(categoryObject).forEach(category => {
+                let filterCategory = document.createElement("li");
+                filterCategory.innerText = `${category} (${categoryObject[category].length})`;
+                listCategory.appendChild(filterCategory);
+                productLine.addEventListener("click", function () {
+                    deleteAllChilds(productsContainer);
+                    let productsFiltered = category.categoryName;
+                    productsFiltered.forEach(productFiltered => {
+                        productCard = document.createElement('div');
+                        productCard.classList.add("item");
+                        productCard.innerHTML = `<img src=${productFiltered.image} alt=""/>
+                                                 <h2>${productFiltered.name}</h2>
+                                                 <p>${productFiltered.description}</p>
+                                                 <h3>${Math.round(productFiltered.price)} $</h3>`;
+                        productsContainer.appendChild(productCard);
+                    });
+                });
+            });*/
         });
     });
 });
 
-
+function deleteAllChilds(parentNode) {
+    while(parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+}
 
 
 //Módulo percentage bar
@@ -48,3 +119,9 @@ function calcPercentage() {
     let percent = Math.round(scrollPercent * 100);
     return percent;
 }
+
+
+
+
+
+
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,6 @@
 async function loadProducts(numProducts) {
     try{
-        let url = `https://fakerapi.it/api/v1/products?_quantity=${numProducts}&_taxes=12&_categories_type=uuid`;
+        let url = `https://fakerapi.it/api/v1/products?_quantity=${numProducts}&_taxes=12&_categories_type=string`;
         let response = await fetch(url);
         if (response.ok) {
             var responseData = await response.json();
@@ -16,3 +16,32 @@ async function loadProducts(numProducts) {
    
 }
 
+function getCategoryList(apiResponseData){
+    // Creamos un objeto para poder tener la categoria como nombre y la relacion de la cantidad de productos que tiene esda categoria
+    let categoriesPlusAmount = {};
+    // Para el array que me da la api de productos. Del tipo [product1,prodct2,...] Itero para acceder a alas proiedades de productn
+    apiResponseData.forEach(productInfo => {
+        //Aqui estaria dentro de 1 producto, ej: product1. Que sabemos que tiene una lista de categorias
+        // para poder coger cada una de las categorias, tiero sobreproductInfo.categories
+        productInfo.categories.forEach(categoryName=> {
+            // Comprobamos si una categoria. EJ: CategoriaX; Esta dentro del objeto categoriesPlusAmount.
+            // PAra eso usamos Object.keys(objeto) que nos da un resultado de todas las categorias que existen dentro de ese objeto.
+            // si no existe, agregamos una nueva con el valor 1 puesto que la hemos encontrado por primera vez
+            /*
+            -- Object.keys(categoriesPlusAmount) 
+                Lista de categorias de los productos
+            */
+           
+            if (! Object.keys(categoriesPlusAmount).includes(categoryName)) {
+                //definimos un array para poder guardar los objetos enteros
+                categoriesPlusAmount[categoryName] = [];
+                categoriesPlusAmount[categoryName].push(productInfo); //Asignamos al value del key un array de productos
+            }else{
+                // SI la encontramos, es decir, si la categoria existe dentro del objeto, agregamos 1 unidad mas, porque hemos encontramos otro objeto mas
+                //Hacemos push a la lista de productos de la categoria
+                categoriesPlusAmount[categoryName].push(productInfo);
+            }
+        });
+    });
+    return categoriesPlusAmount;
+}
